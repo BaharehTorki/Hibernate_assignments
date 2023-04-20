3685c1a5c403b9fe8bceed2724ea1a1e061fdd99
BaharehTorki <bahareh.torki@yh.nackademin.se>
2023-04-20 17:43:09 +0200

    Add an object of Friend

--- src/main/java/com/example/complexhibernate/ComplexHibernateApplication.java
+++ src/main/java/com/example/complexhibernate/ComplexHibernateApplication.java
@@ -1,5 +1,7 @@
 package com.example.complexhibernate;
 
+import com.example.complexhibernate.assignment.models.Friend;
+import com.example.complexhibernate.assignment.repos.FriendRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,4 +15,14 @@ public class ComplexHibernateApplication {
 		SpringApplication.run(ComplexHibernateApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo(FriendRepository repository){
+		return (args) -> {
+			repository.save(new Friend("Robin", "aaaaaa", "887877777"));
+			repository.save(new Friend("Maria", "bbbbbbb", "0755785456"));
+			repository.save(new Friend("Ida", "ccccccc", "67678346"));
+			repository.save(new Friend("Anna", "ddddddddd", "743452122"));
+			repository.save(new Friend("David", "fffffffff", "543564565"));
+		};
+	}
 }
